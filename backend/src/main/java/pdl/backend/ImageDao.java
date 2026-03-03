43b1e4a1c03f359cf444f2dd70d3b73fb4702ccb
hemlys mobio <hmobio@emi.u-bordeaux.fr>
2026-03-03 16:52:18 +0100

    Création de la classe ImageDao implémentant Dao<Image> Stockage des images dans une structure en mémoire Implémentation des méthodes : create : ajout d'une image retrieve : récupération par id retrieveAll : retourne toutes les images update : mise à jour des attributs si nécessaire delete : suppression d'une image Objectif : séparer la logique métier (ImageService) de l'accès aux données.

---
+++ backend/src/main/java/pdl/backend/ImageDao.java
@@ -0,0 +1,33 @@
+package pdl.backend;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class ImageDao implements Dao<Image> {
+    private final Map<Long, Image> images = new HashMap<>();
+
+    @Override
+    public void create(Image image) {
+         images.put(image.getId(), image); 
+        }
+
+    @Override
+    public Optional<Image> retrieve(long id) {
+         return Optional.ofNullable(images.get(id)); 
+    }
+
+    @Override
+    public List<Image> retrieveAll() { 
+        return new ArrayList<>(images.values());
+     }
+
+    @Override
+    public void update(Image image, String[] params) { }
+
+    @Override
+    public void delete(Image image) {
+         images.remove(image.getId()); 
+        }
+}
