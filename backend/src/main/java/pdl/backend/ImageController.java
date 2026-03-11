5922567cfc3c985dacc38a8c2a25b0f2a41ad694
Jean Musango Sele <jean.musango-sele@etu.u-bordeaux.fr>
2026-03-11 12:42:51 +0100

    Implémentation de l’endpoint GET /images (Besoin 6) : Ajout de la classe ImageController pour gérer les requêtes HTTP liées aux images. Implémentation de l’endpoint GET /images qui récupère les images via ImageDao et retourne la liste de leurs métadonnées au format JSON conformément au Besoin 6.

---
+++ backend/src/main/java/pdl/backend/ImageController.java
@@ -0,0 +1,52 @@
+package pdl.backend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.List;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RestController
+public class ImageController {
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  private final ImageDao imageDao;
+
+  public ImageController(ImageDao imageDao) {
+    this.imageDao = imageDao;
+  }
+
+
+  @RequestMapping(value = "/images", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+  @ResponseBody
+  public ArrayNode getImageList() {
+    ArrayNode nodes = mapper.createArrayNode();
+
+    List<Image> images = imageDao.retrieveAll();
+    for (Image image : images) {
+      ObjectNode node = mapper.createObjectNode();
+      node.put("name", image.getName());
+      node.put("id", image.getId());
+      nodes.add(node);
+    }
+    return nodes;
+  }
+
+}
--- backend/src/main/java/pdl/backend/ImageDao.java
+++ backend/src/main/java/pdl/backend/ImageDao.java
@@ -1,7 +1,6 @@
 package pdl.backend;
 
 import org.springframework.stereotype.Repository;
-
 import java.util.*;
 
 @Repository
